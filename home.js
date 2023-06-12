ba0770eda809b23418e63baf7717012c039e07c4
jeremyHarrell <hillvulf@gmail.com>
2023-06-12 12:54:35 -0500

    initiate grid with loops

--- home.js
+++ home.js
@@ -1,27 +1,23 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('.container')
 
 
 const createDiv = () => {
+
+  for (let i = 0; i < 16; i++){
+    const row = document.createElement('div')
+    row.classList.add('row')
+    container.appendChild(row)
+    row.textContent = 'X'
+
+    for (j =0; j<16; j++){
+      const col = document.createElement('div');
+      col.className = 'col';
+      row.appendChild(col)
+      col.textContent = 'O'
+    }
+  }
   
-  const squareDiv = document.createElement('div')
-  squareDiv.classList.add('squareDiv')
-  squareDiv.textContent = 'this a squareDiv'
-  container.appendChild(squareDiv)
 }
 
 createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
-createDiv()
+
