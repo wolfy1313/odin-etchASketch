f4e9cef52d2874ba82c92edf26bb25a37bc11fde
jeremyHarrell <hillvulf@gmail.com>
2023-06-12 12:28:55 -0500

    create container and square divs

---
+++ home.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('#container')
+
+
+const createDiv = () => {
+  
+  const squareDiv = document.createElement('div')
+  squareDiv.classList.add('squareDiv')
+  squareDiv.textContent = 'this a squareDiv'
+  container.appendChild(squareDiv)
+}
+
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
+createDiv()
