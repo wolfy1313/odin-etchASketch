dcdad3cba931c23a99f5ec54e6d5b69b478da5eb
jeremyHarrell <hillvulf@gmail.com>
2023-06-13 13:20:09 -0500

    refactor code to take user input from prompt

--- home.js
+++ home.js
@@ -2,17 +2,28 @@ const container = document.querySelector('.container')
 
 const squaresButton = document.querySelector('.squaresButton')
 
+// squaresButton.addEventListener('onclick', e => alert("how many squares?"))
 
+function numberPopup() {
+  let num = prompt('How many squares?', "0")
+  if (num > 100 || num < 1){
 
-const createDiv = () => {
+    num = prompt("Enter a valid number between 1-100 please", "0")
+    createDiv(num)
+  } else {
+  createDiv(num)
+  }
+}
 
-  for (let i = 0; i < 16; i++){
+const createDiv = (num) => {
+
+  for (let i = 0; i < num; i++){
     const row = document.createElement('div')
     row.classList.add('row')
     container.appendChild(row)
     row.textContent = ''
 
-    for (j =0; j<16; j++){
+    for (j =0; j<num; j++){
       const col = document.createElement('div');
       col.className = 'col';
       row.appendChild(col)
@@ -23,6 +34,6 @@ const createDiv = () => {
   
 }
 
-createDiv()
+createDiv(16)
 
 
